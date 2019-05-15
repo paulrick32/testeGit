633bc2907bd71be11b45b38a1dba8a4a2c7d86ea
Paulo <pr.atg32@gmail.com>
2019-05-15 14:11:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) =>{
     res.status(200).send({message: 'Olá, estou funcionando'});
 })
 
+app.get('/ola', (req, res) =>{
+    res.status(200).send({message: 'oi, eu sou a rota olá'});
+})
+
 app.listen(3001, () =>{
 
             console.log('api rodando na porta 3001');
