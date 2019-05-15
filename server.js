d8878707ab6392f9f84319752509d3005af05f0f
Paulo <paulo@mmda.com.br>
2019-05-15 14:01:37 -0300

    criado o servidor

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) =>{
+    res.status(200).send({message: 'Olá, estou funcionando'});
+})
+
+app.listen(3001, () =>{
+
+            console.log('api rodando na porta 3001');
+})
